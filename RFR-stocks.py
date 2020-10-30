7b0f44df848f51e818f85c11126def9d67a3c366
tuyet-pham <tiipham19@gmail.com>
2020-10-30 00:28:49 -0500

    added dev, train and test folders

--- RFR-stocks.py
+++ RFR-stocks.py
@@ -3,23 +3,44 @@ from sklearn import tree
 import numpy as np
 import matplotlib as plot
 # import tqdm
-# import time
+import time
 pd.options.display.max_rows=10
 
 
-# To get the list of names
-file1 = open("data/listall.csv","w+") 
+# To get the list of Name values from the 'Name' column from the 
 alst = pd.read_csv("data/all_stocks_5yr.csv")
 alst = alst['Name'].unique()
 
+# create and open the listall.csv
+file1 = open("data/listall.csv","wt")
+
+# Column header
 file1.writelines("Name\n")
+
+# Write the Name of stocks
 for e in alst:
-    file1.writelines(e+"\n")
+    file1.writelines(e + "\n")
 file1.close()
-file1 = pd.read_csv('data/listall.csv')
 
 
-def train_dev(name):
+def train_dev_file(dev_set, train_set, stock_name):
+    # for i in tqdm.tqdm(range(200)):
+    #     time.sleep(0.01)
+
+    ts = (r"data/train/" + stock_name + "_train_data.csv")
+    ds = (r"data/dev/" + stock_name + "_dev_data.csv")
+    
+    dev_set.to_csv(ds,index=False)
+    train_set.to_csv(ts,index=False)
+    
+    
+    time.sleep(.2) # comment this out if you're going to use the tqdm library
+    print("Train and Dev set file creation - Done")
+
+
+
+# Getting the a tuple from the file - train and dev set
+def train_dev(stockname):
     
     """[summary]
         Getting a tuple from the file - train and dev set of a specific stock
@@ -28,32 +49,40 @@ def train_dev(name):
     Returns:
         [tuples]: train_stock, dev_stock
     """
-    filepath = 'data/individual_stocks_5yr/' + name + '_data.csv'
+    print("\nStock good: %s\nUsing 2013-2015 for training and 2016-2018.." %stockname)
+    filepath = 'data/individual_stocks_5yr/' + stockname + '_data.csv'
     df = pd.read_csv(filepath)
-    
+
     # Getting within the time
-    train_stock = df[(df['date'] >= '2012-12-31') & (df['date'] <= '2016-01-01')]
-    dev_stock = df[(df['date'] >= '2016-01-02') & (df['date'] <= '2019-01-01')]
+    train_stock = df[(df['date'] > '2012-12-31') & (df['date'] < '2016-01-01')]
+    dev_stock = df[(df['date'] > '2015-12-31') & (df['date'] < '2019-01-01')]
     
     # Just Printing timer for extra coolness
-    print("\nStock good: %s\nUsing: \n2013-2015 for training... \n2016-2018 for Dev.." %name)
     # for i in tqdm.tqdm(range(200)):
     #     time.sleep(0.01)
-
+    
+    
+    time.sleep(.2) # comment this out if you're going to use the tqdm library
+    print("Train and Dev set creation - Done")
     return train_stock, dev_stock
+
 
 
 
 # if we want to do this later I guess
 def train_all():
-    # for i in tqdm.tqdm(range(200)):
-    #     time.sleep(0.01)
     print("Running ALL... might be a minute")
 
 
-
-def pick_stock():
+# Open listall
+try:
+    file1 = pd.read_csv('data/listall.csv')
+except FileExistsError:
+    print('\nFile doesnt exist.')
     
+
+# Picking an individual stock
+def pick_stock():
     """[summary]
         Make the user pick a correct stock
     Returns:
@@ -64,36 +93,41 @@ def pick_stock():
 
     # Get a good name before continuing
     while found == False:
-        stkname = input("Stock Name? ").upper()              
+        stkname = input("Enter stock name or QUIT: ").upper()              
         
-        # finding name
+        if stkname == "QUIT":
+            return ""
+
         for e in file1['Name']:                             
             if e == stkname:
                 return stkname
-
-        if stkname == "QUIT":
-            break
+        
         print("Can't find %s Try again?" %stkname)
 
-    return stkname
+    return ""
+
+
 
 
 train_set = pd.DataFrame()
 dev_set = pd.DataFrame()
 
 if __name__ == "__main__":
+    # Run to make life easier
     all_or_one = ""
     all_or_one = input("Train ALL? *Say no for now* [y/n]: ")
 
     if(all_or_one == "n"):
-        
         stock_name = pick_stock()
-        if(stock_name != ""):
+        if(stock_name):
             train_set, dev_set = train_dev(stock_name)
-        
-        print("\n\nTraing set\n",train_set,"\n\nDevset\n", dev_set)
-
-else:
-    train_all()
+            print("\nTraining set\n",train_set,"\n\nDevset\n", dev_set)
+            
+            # !Important - This will create .csv file of each sets.
+            train_dev_file(dev_set, train_set, stock_name)
+    
+            
+    # else:
+    #     train_all()
 
 
